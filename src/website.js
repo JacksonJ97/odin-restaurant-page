f59d2a964c4781e5cfcef12cb5d1970a541d0f70
JacksonJ97 <jacksonjude97@gmail.com>
2021-08-29 08:49:17 -0400

    added hamburger menu

--- src/website.js
+++ src/website.js
@@ -2,19 +2,54 @@ import loadHomePage from "./home";
 import loadMenuPage from "./menu";
 import loadContactPage from "./contact";
 
+const createHamburgerMenu = () => {
+  const hamburgerBtn = document.createElement("div");
+  const hamburgerMenu = document.createElement("div");
+  hamburgerBtn.classList.add("burger-btn");
+  hamburgerMenu.classList.add("burger-menu");
+
+  hamburgerBtn.append(hamburgerMenu);
+
+  return hamburgerBtn;
+};
+
 const createNav = () => {
   const nav = document.createElement("nav");
+  const companyLogo = document.createElement("div");
+  const companyText = document.createElement("p");
+  const companySlogan = document.createElement("p");
+  const hamburgerMenu = createHamburgerMenu();
   const ul = document.createElement("ul");
   const homeTab = document.createElement("li");
   const menuTab = document.createElement("li");
   const contactTab = document.createElement("li");
 
+  companyText.textContent = "Jack's Cafe";
+  companySlogan.textContent = "One sip at a time";
   homeTab.textContent = "Home";
   menuTab.textContent = "Menu";
   contactTab.textContent = "Contact";
+
+  companyLogo.classList.add("company-logo");
+  companyText.classList.add("company-text");
+  companySlogan.classList.add("company-slogan");
   homeTab.classList.add("nav-button");
   menuTab.classList.add("nav-button");
   contactTab.classList.add("nav-button");
+
+  let menuOpen = false;
+
+  hamburgerMenu.addEventListener("click", () => {
+    if (!menuOpen) {
+      hamburgerMenu.classList.add("open");
+      ul.classList.add("show");
+      menuOpen = true;
+    } else {
+      hamburgerMenu.classList.remove("open");
+      ul.classList.remove("show");
+      menuOpen = false;
+    }
+  });
 
   homeTab.addEventListener("click", (e) => {
     if (e.target.classList.contains("active")) return;
@@ -34,9 +69,13 @@ const createNav = () => {
     loadContactPage();
   });
 
+  companyLogo.append(companyText);
+  companyLogo.append(companySlogan);
   ul.append(homeTab);
   ul.append(menuTab);
   ul.append(contactTab);
+  nav.append(companyLogo);
+  nav.append(hamburgerMenu);
   nav.append(ul);
 
   return nav;
@@ -45,23 +84,10 @@ const createNav = () => {
 const createHeader = () => {
   const header = document.createElement("header");
   const container = document.createElement("div");
-  const companyLogo = document.createElement("div");
-  const companyText = document.createElement("p");
-  const companySlogan = document.createElement("p");
   const nav = createNav();
 
-  companyText.textContent = "Jack's Cafe";
-  companySlogan.textContent = "One sip at a time";
-
-  companyLogo.classList.add("company-logo");
-  companyText.classList.add("company-text");
-  companySlogan.classList.add("company-slogan");
   container.classList.add("container");
 
-  companyLogo.append(companyText);
-  companyLogo.append(companySlogan);
-
-  container.append(companyLogo);
   container.append(nav);
   header.append(container);
 
